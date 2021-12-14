d5a792eae1e382f6d59b9c10aac84c314c53b53a
chethanm933/chethanm933 <chethanm933@gmail.com>
2021-12-14 18:52:40 +0530

    addressbook uc1 completed

---
+++ AddressBook.java
@@ -0,0 +1,42 @@
+package PhoneBook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+	
+	String Name;
+	String Last_Name;
+	String Address;
+	String City;
+	String State;
+	int Zip_code;
+	int Phone_Number;
+	String E_mail;
+	
+	public void newContact(String Name, String Last_Name, String Address, String City, String State, int Zip_code, int Phone_Number, String E_mail)  {
+		
+		this.Name = Name;
+		this.Last_Name = Last_Name;
+		this.Address = Address;
+		this.City = City;
+		this.State = State;
+		this.Zip_code = Zip_code;
+		this.Phone_Number = Phone_Number;
+		this.E_mail = E_mail;
+		
+		System.out.println("Name :"+Name+"\nLast name :"+Last_Name+"\nAddress :"+Address+"\nCity :"+City+"\nState :"+State+"Zip-code :"+Zip_code+"\nPhone number :"+Phone_Number+"\nEmail :"+E_mail);
+	}
+
+	public static void main(String[] args) {
+		
+		AddressBook addressbook = new AddressBook();
+		addressbook.newContact("CHETHAN", "M", "BANGLORE", "BANGLORE", "KARNATAKA", 560063, 888888888, "CHETHANM933@GMAIL.COM");
+	
+		addressbook.newContact("kiran", "m b", "smet", "bdvt", "kar", 577301, 888899999, "kiranmb@gmail.com");
+	}
+}
+
+	
+
+	
+
